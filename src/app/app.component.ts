5d5a394eaabc53c82b87a9a234da8310e7884f74
Luis Gines <legines@gmail.com>
2022-05-28 17:23:54 -0400

    Updating circle pic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'portfolio';
   urls = ['./assets/ended.png', './assets/cov-api.png', './assets/gp.png', './assets/hangman.png'];
+  img_url = './assets/IMG_1584.jpg';
 }
